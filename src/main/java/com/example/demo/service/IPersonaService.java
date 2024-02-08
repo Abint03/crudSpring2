3c41a4d7737581915b13c758b765d2c890635813
Abigail Alegre <129190234+Abint03@users.noreply.github.com>
2024-02-08 19:17:42 -0300

    Persona service
    Interface implementation in personas's service

--- src/main/java/com/example/demo/service/IPersonaService.java
+++ src/main/java/com/example/demo/service/IPersonaService.java
@@ -1,4 +1,13 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Persona;
+
+import java.util.List;
+
 public interface IPersonaService {
+    public List<Persona> getPersonas();
+    public void savePersona(Persona person);
+    public void deletePersona(Long id);
+    public Persona findPersona(Long id);
+    public void editPersona(Long oldId, Long newId, String newName, String newSurname, int newAge);
 }
--- src/main/java/com/example/demo/service/PersonaService.java
+++ src/main/java/com/example/demo/service/PersonaService.java
@@ -1,13 +1,50 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Persona;
 import com.example.demo.repository.IPersonaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class PersonaService {
+public class PersonaService implements IPersonaService{
     @Autowired
     private IPersonaRepository persoRepo;
+
+    @Override
+    public List<Persona> getPersonas() {
+        List<Persona>personList=persoRepo.findAll();
+
+        return personList;
+    }
+
+    @Override
+    public void savePersona(Persona person) {
+        persoRepo.save(person);
+    }
+
+    @Override
+    public void deletePersona(Long id) {
+        persoRepo.deleteById(id);
+    }
+
+    @Override
+    public Persona findPersona(Long id) {
+        Persona p=persoRepo.findById(id).orElse(null);
+        return p;
+    }
+
+    @Override
+    public void editPersona(Long oldId, Long newId, String newName, String newSurname, int newAge) {
+        Persona p=this.findPersona(oldId);
+        p.setId(newId);
+        p.setNombre(newName);
+        p.setApellido(newSurname);
+        p.setEdad(newAge);
+
+        this.savePersona(p);
+    }
 
 
 }
